1fcf943bbebfe18c7665451bfe4df22a4b97684f
eliesjebri <eliesjebri@users.noreply.github.com>
2023-03-05 22:53:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,7 @@ import
    "net/http"
 )
 type Server struct{}
-func (s *Server) ServeHTTP(w http.ResponseWriter, r 
-*http.Request) {
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
    w.Header().Set("Content-Type", "application/json")
    w.Write([]byte(`{"message": "hello world"}`))

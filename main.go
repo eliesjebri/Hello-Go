2b55ed6236a877fe20a9e44b1e64fe986cc9ed1d
eliesjebri <eliesjebri@users.noreply.github.com>
2023-03-06 00:37:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
-  
+
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
-const (
-  port = ":8090"
-)
-
-var calls = 0
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-  calls++
-  fmt.Fprintf(w, "Hello, world! You have called me %d times.\n", calls)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+	http.ListenAndServe(":8090", nil)
 }
-
-func init() {
-  fmt.Printf("Started server at http://localhost%v.\n", port)
-  http.HandleFunc("/", HelloWorld)
-  http.ListenAndServe(port, nil)
-}
-
-func main() {}
